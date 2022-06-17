c87f91c77de946f4e47a68af589892ef1152f1b4
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2022-06-17 12:16:35 -0300

    'torch'

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity , Alert} from 'react-native';
+import { useState, useEffect } from 'react';
+import Torch from 'react-native-torch';
 
 export default function App() {
+
+  const [toogle, setToogle] = useState(false)
+  useEffect(()=>{
+   Torch.switchState(toogle)
+     
+  }, [toogle])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <TouchableOpacity style={toogle ? styles.containerLight : styles.container } onPress={() => setToogle(!toogle)}>
+          <Image style={toogle ? styles.lightinOn : styles.lightinOff} source={ toogle ? require('./assets/eco-light.png') :require('./assets/eco-light-off.png')}/>
+
+          <Image style={styles.dioLogo} source={ toogle ? require('./assets/logo-dio.png') :require('./assets/logo-dio-white.png')}/>
+      
+      </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  containerLight :{
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  lightinOn : {
+    resizeMode : 'contain',
+    alignSelf : 'center',
+    width : 150,
+    height : 150
+  },
+
+  lightinOff : {
+    resizeMode : 'contain',
+    alignSelf : 'center',
+    tintColor : 'white',
+    width : 150,
+    height : 150
+  },
+ dioLogo : {
+    resizeMode : 'contain',
+    alignSelf : 'center',
+  
+    width : 250,
+    height : 250
+  }
 });
